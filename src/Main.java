cfa1d2f513310132c326be4b3b74902269566719
amanzhol <rysbek_dauren@mail.ru>
2023-05-23 23:05:00 +0600

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        BST<Integer, Integer> bst = new BST<>();
+
+        for (int i = 0; i < 12; i++) {
+            bst.put(i, i+1);
+        }
+
+        Iterable it = bst.iterator();
+        for(Object el: it) {
+            System.out.println(el);
+        }
     }
 }
